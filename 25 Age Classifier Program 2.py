127c311a3743b731c2ff7c15fc44756e8aa1c62a
CalebSaari <saaricaleb32@gmail.com>
2025-02-13 20:06:44 -0600

    Update 25 Age Classifier Program 2.py
    Added  pseudocode

--- 25 Age Classifier Program 2.py
+++ 25 Age Classifier Program 2.py
@@ -1,4 +1,8 @@
-age=int(input("Enter someone's age:"))
+# Write a program that asks the user to enter a person's age.  The program should display a message indicating whether the person is an infant, a child, a teenager, or an adult.  Following are the guidelines:
+# If the person is 1 year old or less, it should display "infant" (without quotes).
+# If the person is older than 1 year, but younger than 13 years, it should display "child".
+# If the person is at least 13 years old, but less than 20 years old, it should display "teenager".
+# If the person is at least 20 year old, it should display "adult".age=int(input("Enter someone's age:"))
 
 if age<=1:
     print(f"infant")
